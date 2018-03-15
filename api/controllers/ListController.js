26d7906e27a4b1b051461bddc083a2c514aac65f
Muhammad Umer <mumer271992@gmail.com>
2018-03-16 04:26:39 +0500

    Max dictionary score 25 set

--- api/controllers/ListController.js
+++ api/controllers/ListController.js
@@ -96,7 +96,7 @@ module.exports = {
                             return item.word === keys[j];
                         });
                         if(found){
-                            words_map[keys[j]].word_score = ( found.score > 15 ? 15 : found.score ) * words_map[keys[j]].score;
+                            words_map[keys[j]].word_score = ( found.score > 25 ? 25 : found.score ) * words_map[keys[j]].score;
                         }
                         else{
                             words_map[keys[j]].word_score = 1;
